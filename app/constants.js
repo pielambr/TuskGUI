a25b76fd119a132d342f5c72de00361ba7f78e9d
Pieterjan <me@pielambr.be>
2015-05-29 18:28:20 +0200

    Added verification in splash, start of messages

--- app/constants.js
+++ app/constants.js
@@ -1,5 +1,6 @@
 module.exports = {
   TUSK_ROOT: "https://www.pielambr.be/tusk/",
   USER_ENDPOINT: "user/",
-  SESSION_ENDPOINT: "session/"
+  SESSION_ENDPOINT: "session/",
+  CONVERSATION_ENDPOINT: "conversation/"
 }
--- browser/splash/splash.js
+++ browser/splash/splash.js
@@ -1,17 +1,21 @@
 var $ = require('jquery');
 var ipc = require('ipc');
+var constants = require('../../app/constants');
 
 var logo;
+var retries;
 $(document).ready(function() {
   logo = $("#tusk_logo img");
+  retries = 0;
   animateLogo();
   setTimeout(function() {
     if(localStorage.getItem("username") && localStorage.getItem("session")){
+      $("#status").text("Checking login...");
       checkLogin();
     } else {
       ipc.send('showLogin');
     }
-  }, 5000);
+  }, 3000);
 });
 
 function animateLogo() {
@@ -20,6 +24,39 @@ function animateLogo() {
   });
 };
 
+function getBasicAuth() {
+  var creds = localStorage.getItem("username") + ":" + localStorage.getItem("session");
+  return "Basic " + btoa(creds);
+}
+
 function checkLogin() {
-  $("#status").text("Checking login...");
+  if(retries < 5) {
+    var timeout = setTimeout(function() {
+      ajax.abort();
+      $("#status").text("Login failed... Retry #" + retries + 1);
+      checkLogin();
+    }, 5000);
+    var ajax = $.ajax({
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader ("Authorization", getBasicAuth());
+      },
+      url: constants.TUSK_ROOT + constants.CONVERSATION_ENDPOINT,
+      success: function() {
+        clearTimeout(timeout);
+        ipc.send('showMessages');
+      },
+      error: function() {
+        clearTimeout(timeout);
+        $("#status").text("An error happened while logging in, please login again");
+        setTimeout(function(){
+          ipc.send('showLogin');
+        }, 2000);
+      }
+    });
+  } else {
+    $("#status").text("Maximum retries failed, redirecting to login...");
+    setTimeout(function() {
+      ipc.send('showLogin');
+    }, 2000);
+  }
 };
